ecc2d53311169100fe4f900d9c1cd98d2ece2893
kjmfum <kjmfum@yahoo.co.uk>
2023-01-23 05:43:11 +0000

    fix no todo to fetch error

--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -3,33 +3,25 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
-
-// import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
-// import { getUserId } from '../utils';
 import { getTodosForUser } from '../../businessLogic/todos';
 import { getUserId } from '../utils';
+import { createLogger } from '../../utils/logger';
 
 // TODO: Get all TODO items for a current user
-
-// const docClient = new AWS.DynamoDB.DocumentClient()
-
-// const todosTable = process.env.todosTable
-// const todosIndexName = process.env.TODOS_CREATED_AT_INDEX
-
+const logger = createLogger(`Fetching All Todos`)
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     // Write your code here
     const userId = getUserId(event)
-    const todos = await getTodosForUser(userId)
-    console.log(`Getting stuff 1: ${todos}`);
-    console.log(`Getting stuff 2: ${todos[0].name}`);
-    console.log(`Getting stuff 3: ${todos[0].userId}`);
-    console.log(`Getting stuff 4: ${todos[0]["name"]}`);
+    let todos = await getTodosForUser(userId)
     
-    // google-oauth2|101675378332843725088
-    // const todos = getTodosForUser(userId)
-    // const todos = getTodosForUser("google-oauth2|101675378332843725088")
+    
+    if(todos.length === 0 || todos !== undefined) {
+      logger.error(`No Todos to fetch`)
+      todos = []
+      
+    }
     
     return {
       statusCode: 200,
@@ -39,22 +31,6 @@ export const handler = middy(
         ]
       })
     }
-
-  //   async function  getTodosForUser(userId: string){
-  //     const result = await docClient.query(
-  //       {
-  //         TableName: todosTable,
-  //         IndexName: todosIndexName,
-  //         KeyConditionExpression: 'userId = :userId',
-  //         ExpressionAttributeValues: {
-  //           ':userId' : userId
-  //         },
-  //         ScanIndexForward:false
-  //       }
-  //     ).promise()
-
-  //     return result.Items;
-  // }
   },
  
   
